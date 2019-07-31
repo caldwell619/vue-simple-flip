a959048cfdfb361f2e25a310990f4e7d0a59689e
Christopher Caldwell <christophercaldwell@IT-33756.attlocal.net>
2019-07-31 10:39:37 -0500

    inital

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="content-window">
+    <FlipCard height="400px" width="300px">
+      <template slot="front">
+        <div class="card-face">
+          <h1>Buy Now?</h1>
+        </div>
+      </template>
+      <template slot="cta-slot-front">
+        <button>CLICK ME!</button>
+      </template>
+      <template slot="back">
+        <div class="card-face">
+          <h1>Buy Later.</h1>
+          <ul>
+            <li>Coverage</li>
+            <li>Coverage</li>
+            <li>Coverage</li>
+            <li>Coverage</li>
+            <li>Coverage</li>
+            <li>Coverage</li>
+            <li>Coverage</li>
+            <li>Coverage</li>
+            <li>Coverage</li>
+            <li>Coverage</li>
+            <li>Coverage</li>
+          </ul>
+        </div>
+      </template>
+      <template slot="cta-slot-back">
+        <button>CLICK ME!</button>
+      </template>
+    </FlipCard>
+  </div>
+</template>
+
+<script>
+import FlipCard from "./vue-simple-flip.vue";
+export default {
+  name: "app",
+  components: {
+    FlipCard
+  },
+  data: function() {
+    return {};
+  }
+};
+</script>
+
+<style>
+.content-window {
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
---
+++ src/vue-simple-flip.vue
@@ -0,0 +1,123 @@
+<template>
+  <div v-bind:class="flipped ? 'flip-container flipped': 'flip-container'">
+    <div class="flipper">
+      <div class="front" v-bind:style="styles">
+        <slot name="front"></slot>
+        <div class="flip-cta-cont" @click="flip">
+          <slot name="cta-slot-front"></slot>
+        </div>
+      </div>
+      <div class="back" v-bind:style="styles">
+        <slot name="back"></slot>
+        <div class="flip-cta-cont" @click="flip">
+          <slot name="cta-slot-back"></slot>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FlipCard",
+  data() {
+    return {
+      flipped: false,
+      styles: {
+        height: this.height,
+        width: this.width,
+        backgroundColor: this.backgroundColor || "#ffffff",
+        color: this.textColor || "black"
+      }
+    };
+  },
+  props: {
+    height: String,
+    width: String,
+    backgroundColor: String,
+    textColor: String
+  },
+  methods: {
+    flip() {
+      console.log("this.height: ", this.height);
+      this.flipped = !this.flipped;
+    }
+  }
+};
+</script>
+
+<style type='text/css' scoped>
+.flip-container {
+  -webkit-perspective: 1000;
+  -moz-perspective: 1000;
+  -o-perspective: 1000;
+  perspective: 1000;
+}
+.flip-container {
+  min-height: 120px;
+}
+.flipper {
+  -moz-transform: perspective(1000px);
+  -moz-transform-style: preserve-3d;
+  position: relative;
+}
+.front,
+.back {
+  -webkit-backface-visibility: hidden;
+  -moz-backface-visibility: hidden;
+  -o-backface-visibility: hidden;
+  backface-visibility: hidden;
+  -webkit-transition: 0.6s;
+  -webkit-transform-style: preserve-3d;
+  -moz-transition: 0.6s;
+  -moz-transform-style: preserve-3d;
+  -o-transition: 0.6s;
+  -o-transform-style: preserve-3d;
+  -ms-transition: 0.6s;
+  -ms-transform-style: preserve-3d;
+  transition: 0.6s;
+  transform-style: preserve-3d;
+  top: 0;
+  left: 0;
+  width: 100%;
+  position: relative;
+  padding: 20px;
+  box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2),
+    0px 3px 7px 0px rgba(0, 0, 0, 0.14), 0px 1px 3px 0px rgba(0, 0, 0, 0.12);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+.back {
+  -webkit-transform: rotateY(-180deg);
+  -moz-transform: rotateY(-180deg);
+  -o-transform: rotateY(-180deg);
+  -ms-transform: rotateY(-180deg);
+  transform: rotateY(-180deg);
+  position: absolute;
+}
+.flip-container.flipped .back {
+  -webkit-transform: rotateY(0deg);
+  -moz-transform: rotateY(0deg);
+  -o-transform: rotateY(0deg);
+  -ms-transform: rotateY(0deg);
+  transform: rotateY(0deg);
+}
+.flip-container.flipped .front {
+  -webkit-transform: rotateY(180deg);
+  -moz-transform: rotateY(180deg);
+  -o-transform: rotateY(180deg);
+  -ms-transform: rotateY(180deg);
+  transform: rotateY(180deg);
+}
+.front {
+  z-index: 2;
+}
+.flip-cta-cont {
+  position: absolute;
+  bottom: 0;
+  right: 0;
+}
+</style>
